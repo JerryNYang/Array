aa43e9cc508c39bacb7c01e6e2b56f00fa26a8de
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2024-04-24 16:46:00 -0500

    Update FirstApp.kt

--- src/FirstApp.kt
+++ src/FirstApp.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 
     val str = "JDenis, Welcome to Kotlin"
-    print(10944020)
+    print(104020)
     print("\nWelcome to kotlin!")
     println("\nJeremiah eDenis")
 
