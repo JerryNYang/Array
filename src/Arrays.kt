ca3b181e1c1c0d5e39775a07efabc5040435f35e
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2024-05-05 15:42:33 -0500

    Update Arrays.kt

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -11,7 +11,7 @@ fun main() {
 
     val myARRAY = arrayOfNulls<String>(6)
     myARRAY[0] = "724"
-    myARRAY[1] = "44"
+    myARRAY[1] = "443"
     myARRAY[2] = "32323"
     myARRAY[3] = "777"
     myARRAY[4] = "Hello"
